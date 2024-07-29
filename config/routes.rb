93bd67083891b6b4b5bb826218b0083935a64646
akshaysharma-advanced <akshay.sharma@oneadvanced.com>
2024-07-29 20:00:36 +0530

    set-correct-routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-class Comment < ApplicationRecord
-  belongs_to :post
-  validates :body, presence: true
+Rails.application.routes.draw do
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+  root "posts#index"
 end
